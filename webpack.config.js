6f603ebea8857a6c3f7c8c4949b6a5ee429fdfb7
kaulysien <kaulysien@gmail.com>
2024-05-20 15:12:19 -0300

    git commit -m sprint 9.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,55 @@
-// webpack.config.js
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  devtool: 'inline-source-map',
-  entry: {
-    main: './src/pages/index.js'
-  },
+  entry: './src/index.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
   },
-  target: ['web', 'es5'],
-  stats: { children: true },
-  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'),
-    compress: true,
-    port: 8080,
-    open: true
+    contentBase: './dist',
+    open: true,
   },
   module: {
-    rules: [ // esse é um vetor de regras
-      // adicione um objeto contendo regras para Babel nele
+    rules: [
       {
-        // uma expressão regular que busca por todos os arquivos js
         test: /\.js$/,
-        // todos os arquivos devem ser processados pelo babel-loader
-        loader: "babel-loader",
-        // exclua a pasta node_modules, não precisamos processar os arquivos nela
-        exclude: "/node_modules/"
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1
-            }
-          },
-          "postcss-loader"
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
-        // adicione a regra para processar arquivos
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource"
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
       },
-    ]
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html" // caminho para nosso arquivo index.html
-    }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin() // conecte o plugin para unir os arquivos CSS
-  ]
-}
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+};
