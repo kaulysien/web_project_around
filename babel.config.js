ed6dc9dabb68627c6b1c7955a151e10a9a712fe9
kaulysien <kaulysien@gmail.com>
2024-05-25 16:20:01 -0300

    git commit -m sprint 9.11

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
-const presets = [
-    [
-      "@babel/env",
-      {
-        targets: {
-          edge: "17",
-          ie: "11",
-          firefox: "50",
-          chrome: "64",
-          safari: "11.1",
-        },
-        useBuiltIns: "entry",
-      },
-    ],
-  ];
-  
-  module.exports = { presets };
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,55 @@
-const path = require("path"); 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: { main: "./src/pages/index.js" },
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: "/node_modules/",
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
-        loader: "file-loader",
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        loader: [
-          MiniCssExtractPlugin.loader,
-          { 
-            loader: "css-loader", 
-          options: { 
-            importLoaders: 1 
-          } 
-        },
-          "postcss-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
       },
     ],
   },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
   ],
 };
